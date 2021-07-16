72e9e89effecdd074fee2aa81dd6bb704eccd0f3
qamk <qamking@gmail.com>
2021-07-16 17:57:14 +0100

    Add model relations and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+
+  belongs_to :user
+  
+  validates :body_word_count, length: { within: 1..250 }
+  validates :title_word_count, length: { within: 1..20, message: 'Must be between 1 and 20 words long' }
+  
+
+  protected
+
+  def title_word_count
+    title.scan(/\w+/)
+  end
+
+  def body_word_count
+    body.scan(/\w+/)
+  end
+
+
 end
